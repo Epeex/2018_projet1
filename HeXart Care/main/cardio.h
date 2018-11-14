8c5d8b64ed238dd99bebcece917eeabd14ce986e
Zundel Stéphane <zundel.stephane@sfr.fr>
2018-11-14 11:07:03 +0100

    Mise en place d'un programme "fonctionnel" qui permet de renvoyer les valeurs du pouls en fonction du temps !!!!!NON TESTE!!!!

---
+++ HeXart Care/main/cardio.h
@@ -0,0 +1,21 @@
+#ifndef cardio.h
+#define cardio.h
+
+
+
+int pulse_a (int mil);
+int pulse_b (int mil);
+int mstobpm (int ms);
+
+
+/*
+
+  int bpm(int PR);
+
+  int pulse(int PR);
+
+
+*/
+
+
+#endif
--- HeXart Care/main/main.ino
+++ HeXart Care/main/main.ino
@@ -1,12 +1,42 @@
-#include <stdio.h>
-#include "cardio.c"
-#include "Arduino.h"
+#include "cardio.h"
+
+typedef struct pouls {
+  int bpm;
+  int time;
+} Pouls;
+
+int a, b, c ;
+const int SEUIL = 50;
+
+Pouls pouls;
 
 void setup() {
-    pinMode(A0, INPUT)
+  pinMode("A0", INPUT);
 }
 
 
-void loop(){
+void loop() {
+
+  c = 0;
+  do {                                            //Tant que c=0 on continue la boucle quipermet de recupérer c qui est le delta entre 2 battements.
+    if (!a && analogRead(0) > SEUIL) {
+      a = pulse_a(analogRead(0), millis());
+    }
+    if (!b && a && analogRead(0) > SEUIL) {
+      b = pulse_b(analogRead(0), millis());
+    }
+    if (a && b) {
+      c = b - a;
+    }
+  } while (!c);
+  a = 0;
+  b = 0;
+
+
+  pouls.bpm = mstobpm(c);
+  pouls.time = millis();
+
+  Serial.print(pouls.time);
+  Serial.print(pouls.bpm);
 
 }
