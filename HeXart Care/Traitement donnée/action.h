32d89f8da0a41c14b29d3ae3c896e47c89eb6bcf
Zundel Stéphane <zundel.stephane@sfr.fr>
2018-11-16 10:51:03 +0100

    Ajout des fonctions de tri

--- HeXart Care/Traitement donnée/action.h
+++ HeXart Care/Traitement donnée/action.h
@@ -9,7 +9,10 @@ typedef struct Ligne {
 	int poul;
 } Ligne;
 
-void tri(Ligne tab[n],int n);
+void tri_croissant_pouls(Ligne *tab, int n);
+void tri_decroissant_pouls(Ligne *tab, int n);
+void tri_croissant_temps(Ligne *tab, int n);
+void tri_decroissant_temps(Ligne *tab, int n);
 int searchmilis(Ligne*,int,int,int*,int);
 int searchpoul(Ligne*,int,int,int*,int);
 int searchmilisborn(Ligne*,int,int,int,int*,int);
