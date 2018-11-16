f1cbbf8f1735cf45b2c64d9c7eae377ad0bbade1
Mathieutamere <mathieulassurguere@hotmail.fr>
2018-11-16 11:00:41 +0100

    Uplouad action

--- HeXart Care/Traitement donnée/action.h
+++ HeXart Care/Traitement donnée/action.h
@@ -9,10 +9,7 @@ typedef struct Ligne {
 	int poul;
 } Ligne;
 
-void tri_croissant_pouls(Ligne *tab, int n);
-void tri_decroissant_pouls(Ligne *tab, int n);
-void tri_croissant_temps(Ligne *tab, int n);
-void tri_decroissant_temps(Ligne *tab, int n);
+void tri(Ligne tab[n],int n);
 int searchmilis(Ligne*,int,int,int*,int);
 int searchpoul(Ligne*,int,int,int*,int);
 int searchmilisborn(Ligne*,int,int,int,int*,int);
